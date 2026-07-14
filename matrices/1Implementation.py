df8c1d437eab2a364e5679d201d43f0932bd7064
Parveen <parveeenrao7727@gmail.com>
2026-07-14 22:20:10 +0530

    matrices

---
+++ matrices/1Implementation.py
@@ -0,0 +1,20 @@
+matrix = [
+
+
+    [1 , 2, 3],
+    [4, 5, 6],
+    [7 ,8, 9]
+]
+
+print(matrix[0][1])  # 2
+
+print(matrix[1][2]) # 5
+
+
+# Number of rows ,
+
+print(len(matrix))
+
+# Number of columns 
+
+print(len(matrix[0]))
